ab63f33680194c67e004513f63711cb8fb617ed9
2tmdjun <2tmdjun@gmail.com>
2023-06-19 15:40:50 +0900

    "프로젝트 제작 설명" by LSJ

---
+++ 프로젝트 제작 설명1/src/projectRecoveryTest.java
@@ -0,0 +1,16 @@
+public class projectRecoveryTest {
+	
+	public static void main(String[] args) {
+
+		System.out.println("주어진 자료를 기반으로 복원하여");
+		System.out.println("개발 환경을 구축할 수 있다");
+		
+//		파일명: 프로젝트 제작 설명.txt
+//		
+//		내용:
+//			
+//		현재날짜: 2023-06-19
+//		
+//		제작자: LSJ
+	}
+}
