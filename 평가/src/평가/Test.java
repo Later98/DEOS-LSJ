233a2b284dcd9570440cf66bd14c48eb9c051105
2tmdjun <2tmdjun@gmail.com>
2023-06-19 15:26:22 +0900

    "프로젝트 제작 설명" by LSJ

---
+++ 평가/src/평가/Test.java
@@ -0,0 +1,15 @@
+package 평가;
+
+public class Test {
+
+	public static void main(String[] args) {
+//		파일명: 프로젝트 제작 설명.txt
+//		
+//		내용: 
+//				
+//		현재날짜: 2023-06-19
+//				
+//		제작자: LSJ
+	}
+
+}
